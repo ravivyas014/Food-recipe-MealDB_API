a37d35e77e59f5eab401500e758920b30584bf11
Ravi vyas <ravivyas014@gmail.com>
2025-03-07 17:21:59 +0530

    Added meal display UI and filter buttons

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Meal from "./Project2/Meal";
+
+const App = () => {
+    
+    
+  return (
+    <>
+    <Meal />
+    </>
+  )
+}
+
+export default App
--- src/Project2/Meal.jsx
+++ src/Project2/Meal.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Meal = () => {
+  const [mealData, setMealData] = useState([])
+  useEffect(() => {
+
+    const fetchDataFromAPI = async () => {
+      const api = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian');
+      const data = await api.json()
+      console.log(data.meals)
+      setMealData(data.meals)
+    };
+    fetchDataFromAPI()
+
+  }, [])
   return (
-    <div>Meal</div>
+    <>
+     <div className="my-3" style={{ width: "1000px", margin: "auto" }}>
+        <div className="mx-auto text-center">
+          <button
+            onClick={() =>setMovieList(movies)}
+            type="button"
+            className="btn btn-outline-primary mx-3"
+          >
+            Indian
+          </button>
+          <button
+            // onClick={() => filterByCategory("Action")}
+            type="button"
+            className="btn btn-outline-warning mx-3"
+          >
+           Canadian
+          </button>
+          <button
+            // onClick={() => filterByCategory("Thriller")}
+            type="button"
+            className="btn btn-outline-light mx-3"
+          >
+            American
+          </button>
+          <button
+            // onClick={() => filterByCategory("Animation")}
+            type="button"
+            className="btn btn-outline-info mx-3"
+          >
+            Thai
+          </button>
+          <button
+            // onClick={() => filterByCategory("Horror")}
+            type="button"
+            className="btn btn-outline-warning mx-3"
+          >
+            British
+          </button>
+          <button
+            // onClick={() => filterByCategory("Drama")}
+            type="button"
+            className="btn btn-outline-info mx-3"
+          >
+            Russian
+          </button>
+         
+        </div>
+      </div>
+ 
+   
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexWrap:"wrap"
+    }}>
+      {mealData.map((data) => <div key={data.idMeal} style={{ textAlign: 'center' }}>
+        <div>
+          <img src={data.strMealThumb} alt="" style={{ width: '220px', borderRadius: '10px', border: '2px solid yellow' }} />
+        </div>
+        <h5>{data.strMeal}</h5>
+      </div>)}
+    </div>
+    </>
   )
 }
 
